c6507ed3d588e089af95ff0fdd958b0cebd8e164
Emeafu Francis Ebere <freemandigits@gmail.com>
2020-11-07 23:57:33 +0100

    initial commit

---
+++ nisoms/local_settings.py
@@ -0,0 +1,12 @@
+
+DEBUG = True
+
+SECRET_KEY = 'm_r_b&1579#yyt%u9x3^2&1+k^ie*@9)i697+fck1h(56kaslo'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
