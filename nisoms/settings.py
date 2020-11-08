9e45fcebe24cc50b3f642296e2bfffc53e9880f8
Emeafu Francis Ebere <freemandigits@gmail.com>
2020-11-08 07:22:03 +0100

    debug

--- nisoms/settings.py
+++ nisoms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'q+2r9lm-5ss6b#p)!ej93321qu&debzihzx@sgtum!)&tzyva1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.nisoms.com', 'nisoms.com']
 
